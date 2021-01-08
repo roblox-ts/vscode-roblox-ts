34edbe0e9020f606f01d1828f84e3ddf10977e00
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-01-07 22:40:46 -0500

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+console.log("test");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext) {
+    const extension = vscode.extensions.getExtension("vscode.typescript-language-features");
+    if (!extension) {
+        return console.log("extension failed");
+    }
+
+    await extension.activate();
+    if (!extension.exports || !extension.exports.getAPI) {
+        return console.log("extension exports failed");
+	}
+
+	const api = extension.exports.getAPI(0);
+    if (!api) {
+        return console.log("extension api failed");
+	}
+	configurePlugin(api);
+
+	vscode.workspace.onDidChangeConfiguration((e) => {
+        if (e.affectsConfiguration("roblox-ts") || e.affectsConfiguration("roblox-ts.boundary")) {
+            configurePlugin(api);
+        }
+    }, undefined, context.subscriptions);
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('roblox-ts extensions has loaded');
+}
+
+export function configurePlugin(api: any) {
+	const boundary = vscode.workspace.getConfiguration("roblox-ts.boundary");
+	const paths = vscode.workspace.getConfiguration("roblox-ts.boundary.paths");
+	api.configurePlugin("roblox-ts-extensions", {
+		mode: boundary.get<string>("mode"),
+		useRojo: boundary.get<boolean>("useRojo"),
+		server: paths.get<string[]>("serverPaths"),
+		client: paths.get<string[]>("clientPaths")
+	});
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
