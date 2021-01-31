78d2fd8fe77f78c5b6e9661b225575cbcdf0cb08
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-01-31 16:04:04 -0500

    Comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,26 @@
 import * as vscode from 'vscode';
 
 export async function activate(context: vscode.ExtensionContext) {
+	// Retrieve a reference to vscode's typescript extension.
 	const extension = vscode.extensions.getExtension("vscode.typescript-language-features");
 	if (!extension) {
 		return console.log("extension failed");
 	}
 
+	// Wait for extension to be activated, if not already active.
 	await extension.activate();
 	if (!extension.exports || !extension.exports.getAPI) {
 		return console.log("extension exports failed");
 	}
 
+	// Get the language server's API for configuring plugins.
 	const api = extension.exports.getAPI(0);
 	if (!api) {
 		return console.log("extension api failed");
 	}
 	configurePlugin(api);
 
+	// Reconfigure the plugin when vscode settings change.
 	vscode.workspace.onDidChangeConfiguration((e) => {
 		if (e.affectsConfiguration("roblox-ts")) {
 			configurePlugin(api);
@@ -29,11 +33,13 @@ export async function activate(context: vscode.ExtensionContext) {
 export function configurePlugin(api: any) {
 	const boundary = vscode.workspace.getConfiguration("roblox-ts.boundary");
 	const paths = vscode.workspace.getConfiguration("roblox-ts.boundary.paths");
+
+	// Updates the settings that the language service plugin uses.
 	api.configurePlugin("roblox-ts-extensions", {
-		mode: boundary.get<string>("mode"),
-		useRojo: boundary.get<boolean>("useRojo"),
-		server: paths.get<string[]>("serverPaths"),
-		client: paths.get<string[]>("clientPaths")
+		mode: boundary.get("mode"),
+		useRojo: boundary.get("useRojo"),
+		server: paths.get("serverPaths"),
+		client: paths.get("clientPaths")
 	});
 }
 
