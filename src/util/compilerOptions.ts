691a7b72781479c1b0da55cda611bf5291a80bd1
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2024-04-30 18:20:52 -0400

    Add multi compilation support

--- src/util/compilerOptions.ts
+++ src/util/compilerOptions.ts
@@ -3,10 +3,18 @@ import { dirname } from "path";
 import ts = require("typescript");
 
 export function getCompilerOptionsAtFile(fileName: string): [string, ts.CompilerOptions] | undefined {
-	const result = ts.findConfigFile(dirname(fileName), ts.sys.fileExists);
+	const result = getTsConfigPathAtFile(fileName);
 	if (!result) return undefined;
 
 	const parsed = ts.parseConfigFileTextToJson(result, readFileSync(result).toString());
 	const compilerOptions = (parsed.config.compilerOptions as ts.CompilerOptions);
 	return [result, compilerOptions];
+}
+
+export function getTsConfigPathAtFile(fileName: string) {
+	return ts.findConfigFile(dirname(fileName), ts.sys.fileExists);;
+}
+
+export function getPackageJsonAtFile(fileName: string) {
+	return ts.findConfigFile(dirname(fileName), ts.sys.fileExists, "package.json");
 }
